ba312c321cc176af55b7e89b4c9af981d9b8ef23
Helio <mm.helio009@gmail.com>
2023-02-19 01:37:48 -0400

    Se agrego la funcion jquery para el boton borrar de la vista venta Producto, asi mismo se commento el codigo para que se entienda mejor

--- public/jquery/jqueryVistaVentaProducto.js
+++ public/jquery/jqueryVistaVentaProducto.js
@@ -1,15 +1,36 @@
+
+
+/*
+* Funcion para seleccionar el producto por id y la cantidad y llenarlo 
+* en la tabla de venta o detalle de venta de Productos
+*/
 $("#btn-ventas-aceptar").click(function (e) { 
     e.preventDefault();
     var trstring='<tr id="reg-ventas"><th scope="row" class="text-uppercase">cod1</th><td class="text-uppercase">Arroz con chicharon de pollo</td><td>1</td><td>20</td></tr>'
     $("#filasDetalleVentas").before(trstring);
 });
 
+
+/*
+* Selecciona los tr's y les asigna css background red, para posteriormente ser eliminados.
+*/
+
 $("tbody").on('click',"tr",function(){
     if ($(this).attr("id") == "reg-ventas"){
         $(this).css("background","red");
     }
-    else{
-        
-    }
 });
 
+
+/*
+* Boton para borrar los tr's seleccionados con rojo de la tabla de ventas
+*/
+$("#btn-ventas-borrar").on("click",function (e) { 
+    e.preventDefault();
+    $("tr").each(function(){
+        if($(this).attr("style") == "background: red;")
+        {
+           $(this).remove();
+        }
+    });
+});
