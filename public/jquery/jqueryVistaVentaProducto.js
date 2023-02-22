e126050f6d0f5640c06145237bc85dd6fb7e86a4
Helio <mm.helio009@gmail.com>
2023-02-22 16:27:22 -0400

    Se mejoraron las funciones de los botones de aceptar y borrar de la vista ventana productos, asi mismo se agrego una moda a dicha vista para mostrar mensajes de cualquier indole (errores, informacion, entre otros), asi mismo se captura la tecla enter en el input del efectivivo para que vuelva a calcular los nuevos montos

--- public/jquery/jqueryVistaVentaProducto.js
+++ public/jquery/jqueryVistaVentaProducto.js
@@ -3,7 +3,7 @@ function calcularMontoTotalCompra(montosPlatos)
     let suma=0;
     for (let index = 0; index < montosPlatos.length; index++) 
     {
-        suma = suma+montosPlatos[index];
+        suma = suma+parseInt(montosPlatos[index]);
     }
     return suma;
 }
@@ -13,10 +13,43 @@ function calcularMontoTotalCompra(montosPlatos)
 * Funcion para seleccionar el producto por id y la cantidad y llenarlo 
 * en la tabla de venta o detalle de venta de Productos
 */
-$("#btn-ventas-aceptar").click(function (e) { 
+$("#btn-ventas-aceptar").on("click", function (e) { 
     e.preventDefault();
-    var trstring='<tr id="reg-ventas"><th scope="row" class="text-uppercase">cod1</th><td class="text-uppercase">Arroz con chicharon de pollo</td><td>1</td><td>20</td></tr>'
-    $("#filasDetalleVentas").before(trstring);
+    const subtotal=[];
+    let formCodPlato = $("#codplato").val();
+    let formCantPlato= parseInt($("#cantplato").val());
+    
+    if(formCantPlato>0)
+    {
+        var trstring='<tr id="reg-ventas"><th scope="row" class="text-uppercase">'+formCodPlato+'</th><td class="text-uppercase">Arroz con chicharon de pollo</td><td id="cantidad-ventas-form">'+formCantPlato+'</td><td id="punitario-ventas-form">20</td><td id="subtotal-ventas-form"></td></tr>'
+        $("#filasDetalleVentas").before(trstring);
+
+        $("tr").each(function(){
+            if($(this).attr("id") == "reg-ventas")
+            {
+                if (!($(this).find('#cantidad-ventas-form').text()))
+                {}
+                else{  
+                cantidad=parseInt($(this).find('#cantidad-ventas-form').text());
+                precioPlato=parseInt($(this).find('#punitario-ventas-form').text());
+                $(this).find("#subtotal-ventas-form").text(cantidad*precioPlato);
+                subtotal.push(cantidad*precioPlato);
+                }
+            }
+        });
+
+        let totalCobrar=calcularMontoTotalCompra(subtotal);
+        $("#cobrar-ventas-form").text(totalCobrar);
+        let efectivoRecido=$("#efectivo-ventas-form").val();
+        $("#cambio-ventas-form").text(efectivoRecido-totalCobrar);
+    }
+    else
+    {
+        $("#modalMensajesLabel").text("ERROR EN LA ** CANTIDAD **");
+        $(".modal-body").text("El dato debe ser un NUMERO o mayor a 0 (cero)");
+        $("#modalMensajes").modal("show");
+    }
+    
 });
 
 
@@ -26,7 +59,13 @@ $("#btn-ventas-aceptar").click(function (e) {
 
 $("tbody").on('click',"tr",function(){
     if ($(this).attr("id") == "reg-ventas"){
-        $(this).css("background","red");
+        if($(this).attr("style") == "background: red;")
+        {
+            $(this).removeAttr("style");
+        }
+        else{
+            $(this).css("background","red");
+        }    
     }
 });
 
@@ -36,11 +75,40 @@ $("tbody").on('click',"tr",function(){
 */
 $("#btn-ventas-borrar").on("click",function (e) { 
     e.preventDefault();
+    const subtotal=[];
     $("tr").each(function(){
         if($(this).attr("style") == "background: red;")
         {
            $(this).remove();
         }
+        else{
+           if (!($(this).find('#cantidad-ventas-form').text()))
+           {}
+           else{
+             cantidad=$(this).find('#cantidad-ventas-form').text();
+             precioPlato=$(this).find('#punitario-ventas-form').text();
+             $(this).find("#subtotal-ventas-form").text(cantidad*precioPlato);
+             subtotal.push(cantidad*precioPlato);
+           }
+        }
     });
-    console.log(calcularMontoTotalCompra([10,20,30]));
+    let totalCobrar=calcularMontoTotalCompra(subtotal);
+    $("#cobrar-ventas-form").text(totalCobrar);
+    let efectivoRecido=$("#efectivo-ventas-form").val();
+    $("#cambio-ventas-form").text(efectivoRecido-totalCobrar);
 });
+
+/*
+* Captura la tecla enter en el input  efectivo-ventas-form y vuelve a calcular nuevos valores
+*/
+$("#efectivo-ventas-form").keypress(function(e) {
+    if(e.which == 13) {
+      e.preventDefault();
+      let aCobrar=$("#cobrar-ventas-form").text();
+      let efectivoRecido=$("#efectivo-ventas-form").val();
+      $("#cambio-ventas-form").text(efectivoRecido-aCobrar);
+    }
+});
+
+
+
--- resources/views/ventaProductos.blade.php
+++ resources/views/ventaProductos.blade.php
@@ -27,12 +27,12 @@
             <form action="" method="post">
                 <div class="row">
                     <div class="col-md-3">
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Cod.Pla.">
-                        <label for="formGroupExampleInput">Codigo Plato</label>
+                        <input type="text" class="form-control" id="codplato" placeholder="Cod.Pla.">
+                        <label for="codplato">Cod. Plato</label>
                       </div>  
                       <div class="col-md-3">
-                         <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Cantidad">
-                         <label for="formGroupExampleInput2">Cantidad</label>    
+                         <input type="text" class="form-control" id="cantplato" placeholder="Cantidad">
+                         <label for="cantplato">Cantidad</label>    
                       </div> 
                       <div class="btn col-md-2 bg-success btn-ventas" id="btn-ventas-aceptar">
                         Aceptar
@@ -55,32 +55,34 @@
             <table class="table table-hover">
                 <thead class="thead-dark text-center">
                   <tr>
-                    <th class="col-md-2 bg-success" scope="col">codigo</th>
-                    <th class="col-md-6 bg-info" scope="col">Descripcion</th>
+                    <th class="col-md-1 bg-success" scope="col">codigo</th>
+                    <th class="col-md-5 bg-info" scope="col">Descripcion</th>
                     <th class="col-md-2 bg-warning" scope="col">Cantidad</th>
-                    <th class="col-md-2 bg-danger" scope="col">Monto Bs</th>
+                    <th class="col-md-2 bg-danger" scope="col">P. Unitario Bs</th>
+                    <th class="col-md-2 bg-danger" scope="col">Sub Total Bs</th>
                   </tr>
                 </thead>
                 <tbody class="text-center">
-                  <tr id="reg-ventas">
+                  <!--tr id="reg-ventas">
                     <th scope="row" class="text-uppercase">cod1</th>
                     <td class="text-uppercase">Arroz con chicharon de pollo</td>
-                    <td id="cantidad-ventas-form">1</td>
-                    <td id="monto-ventas-form">20</td>
-                  </tr>
+                    <td id="cantidad-ventas-form">2</td>
+                    <td id="punitario-ventas-form">20</td>
+                    <td id="subtotal-ventas-form">40</td>
+                  </tr-->
                   <tr id="filasDetalleVentas"></tr>
                   <tr>
-                    <th colspan="2"></th>
+                    <th colspan="3"></th>
                     <td>A Cobrar Bs:</td>
-                    <td id="cobrar-ventas-form">60</td>
+                    <td id="cobrar-ventas-form">0</td>
                   </tr>
                   <tr>
-                    <th colspan="2"></th>
+                    <th colspan="3"></th>
                     <td>Efectivo Bs:</td>
-                    <td id="efectivo-ventas-form"><input type="number" placeholder="0" style="width: 50%"></td>
+                    <td><input id="efectivo-ventas-form" value=0 type="number" placeholder="0" style="width: 80%"></td>
                   </tr>
                   <tr>
-                    <th colspan="2"></th>
+                    <th colspan="3"></th>
                     <td>Cambio Bs:</td>
                     <td id="cambio-ventas-form">0</td>
                   </tr>
@@ -89,5 +91,26 @@
         </div>
     </div>
 </div>
+
+<!--Modal de mensajes-->
+<div class="modal fade" id="modalMensajes" tabindex="-1" aria-labelledby="modalMensajesLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalMensajesLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Fin modal de mensajes-->
 @include('plantilla.pie')
 
